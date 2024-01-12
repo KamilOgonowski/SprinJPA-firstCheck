573f6523324b980d5df983f9b7ffc16e4dc6b14d
KamilOgonowski <kml.ogonowski@gmail.com>
2024-01-12 23:44:01 +0100

    bo for vaccine has been added

--- VaccineManagementApp/src/main/java/com/kamil/VaccineManagementAppApplication.java
+++ VaccineManagementApp/src/main/java/com/kamil/VaccineManagementAppApplication.java
@@ -2,12 +2,14 @@ package com.kamil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class VaccineManagementAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VaccineManagementAppApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(VaccineManagementAppApplication.class, args);
+		
 	}
 
 }
